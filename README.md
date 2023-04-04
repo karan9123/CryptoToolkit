3721e59ecc7f5139fdbf02ec92dfb0df81006e16
karan <ka7982@rit.edu>
2023-04-04 13:20:01 -0400

    Square and multiply with docs.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # CryptoToolkit
 Repo for programs related to cryptography
 
-## Square and multiply
+## 1. Square and multiply
+ The square and multiply algorithm is a method to compute modular exponentiation, i.e., calculating __a^p mod n__ for given integers __a, p__, and __n__. It is a more efficient algorithm for performing modular exponentiation compared to the naive method of computing a^p first and then taking the modulus with n.
+
+The square and multiply algorithm reduces the number of multiplications needed to compute a^p mod n. It does so by breaking down the exponent p into its binary representation and performing repeated squaring and modular multiplications based on the bits of the binary representation.
+
--- square_multiply.py
+++ square_multiply.py
@@ -3,34 +3,38 @@ This script calculates the result of a^p mod n using the square and multiply alg
 
 """
 
-def square_multiply(a: int, pw: int, n: int) -> int:
-"""
-Calculates a^p mod n using the square and multiply algorithm.
-Args:
-    a: The base value (a) as an integer.
-    pw: The power value (p) as an integer.
-    n: The mod value (n) as an integer.
 
-Returns:
-    The result of a^p mod n as an integer.
-"""
-k = 1
-pw = bin(pw)
-for i in range(2, len(pw)):
-    k = (k * k) % n
-    if pw[i] == "1":
-        k = (k * a) % n
-return k
+def square_multiply(a: int, pw: int, n: int) -> int:
+    """
+        Calculates a^p mod n using the square and multiply algorithm.
+        Args:
+            a: The base value (a) as an integer.
+            pw: The power value (p) as an integer.
+            n: The mod value (n) as an integer.
+
+        Returns:
+            The result of a^p mod n as an integer.
+    """
+    k = 1
+    pw = bin(pw)
+    for i in range(2, len(pw)):
+        k = (k * k) % n
+        if pw[i] == "1":
+            k = (k * a) % n
+    return k
+
+
 def main() -> None:
-"""
-Main function to get user input and call square_multiply function.
-Prints the result of a^p mod n.
-"""
-print("a^p mod n")
-a = int(input("Enter base value (a): "))
-pw = int(input("Enter Power(p): "))
-n = int(input("Enter Mod value(n): "))
-print(square_multiply(a, pw, n))
+    """
+        Main function to get user input and call square_multiply function.
+        Prints the result of a^p mod n.
+    """
+    print("a^p mod n")
+    a = int(input("Enter base value (a): "))
+    pw = int(input("Enter Power(p): "))
+    n = int(input("Enter Mod value(n): "))
+    print(square_multiply(a, pw, n))
+
 
 if __name__ == '__main__':
     main()
